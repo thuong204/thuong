10c228020fd294d636ccf7b183a38ea7a0a5e72d
ThuongDev <130146047+thuong204@users.noreply.github.com>
2024-10-29 09:15:21 +0700

    Create greet.h

---
+++ greet.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "greet.h"
+int main(void) {
+greet();
+if (setgreeting("Howdy!") != 0) {
+fprintf(stderr, "Unable to set greeting!\n");
+}
+greet();
+hello("world", getgreeting());
+return 0;
+}
